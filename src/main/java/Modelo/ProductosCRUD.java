a1e456c9ed5d93ff4dc87c6522c18aab121e319f
jaimesvlucas <jaimesanchez456@gmail.com>
2021-02-21 12:09:37 +0100

    Version 2 ahora si realiza la conexion con la base de datos

--- src/main/java/Modelo/ProductosCRUD.java
+++ src/main/java/Modelo/ProductosCRUD.java
@@ -4,16 +4,21 @@
  * and open the template in the editor.
  */
 package Modelo;
-import javax.persistence.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 /**
  *
  * @author User
  */
 public class ProductosCRUD {
      public static int destroyProducto(int id) {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("my_persistence_unit");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("com.mycompany_CRUDproductos_war_1.0-SNAPSHOTPU");
         EntityManager manager = factory.createEntityManager();
-        String sql = "DELETE from Productos p WHERE p.id = " + id;
+        String sql = "DELETE from Productos p WHERE p.id =" + id;
         Query q = manager.createQuery(sql);
         manager.getTransaction().begin();
         int filasAfectadas = q.executeUpdate(); //para las consultas de modif. datos se usa el método executeUpdate
